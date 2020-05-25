b2ce0e05f9fbbca04f5c327d14525ebd4002827b
Jyneda <jyneda@gmail.com>
2020-05-26 01:36:08 +0800

    Add files via upload

---
+++ V7/crawl_weather_V7.py
@@ -0,0 +1,24 @@
+import requests
+import time
+from io import open
+
+region = 'BaoShan'
+url = 'https://www.cwb.gov.tw/V7/observe/24real/Data/C0D58.htm'
+
+
+def f(url, fn):
+	headers = {
+     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+	}
+	res = requests.get(url, headers=headers)
+	res.encoding = 'utf-8'
+
+	open(fn,'wb').write(res.text.encode('utf-8'))
+
+fn = region+ '.html'.format(0,0)
+f(url, fn)
+
+
+
+
+
